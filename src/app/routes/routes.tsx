0f06da9f995e81ab63888c6de05a42165acd5581
장강민 <jkm9780533@gmail.com>
2026-07-21 14:11:18 +0900

    feat: routes에 실이동 추가

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,10 +1,12 @@
 import type { Routes } from "@b1nd/aid-kit/navigation";
 import { HomePage } from "../../pages/home";
 import { ProfilePage } from "../../pages/profile";
+import { RoomPage } from "../../pages/room";
 import { TAB_PATHS } from "../../shared/config/tabs";
 
 export const routes: Routes = {
   tabs: [
+    { path: TAB_PATHS.room, element: RoomPage },
     { path: TAB_PATHS.home, element: HomePage },
     { path: TAB_PATHS.profile, element: ProfilePage },
   ],
