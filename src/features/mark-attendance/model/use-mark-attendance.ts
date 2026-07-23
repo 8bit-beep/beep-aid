85fbb06d7da82995873e23dca0aa37d6eda04fe5
장강민 <jkm9780533@gmail.com>
2026-07-24 01:11:10 +0900

    feat: 출석하기/QR찍기에 성공 콜백(onSuccess) 추가
    출석 체크 위젯이 출석 성공 여부를 알 수 있도록 두 기능에
    onSuccess 콜백 파라미터를 추가했다.

--- src/features/mark-attendance/model/use-mark-attendance.ts
+++ src/features/mark-attendance/model/use-mark-attendance.ts
@@ -1,6 +1,7 @@
-export const useMarkAttendance = () => {
+export const useMarkAttendance = (onSuccess?: () => void) => {
   const markAttendance = () => {
     // TODO: 출석 처리 API 연동
+    onSuccess?.();
   };
 
   return { markAttendance };
--- src/features/scan-attendance-qr/model/use-scan-attendance-qr.ts
+++ src/features/scan-attendance-qr/model/use-scan-attendance-qr.ts
@@ -1,10 +1,11 @@
 import { Actions, useBridgeProvider, useBridgeResponse } from "@b1nd/aid-kit/bridge-kit/web";
 
-export const useScanAttendanceQr = () => {
+export const useScanAttendanceQr = (onSuccess?: () => void) => {
   const { send } = useBridgeProvider();
 
   useBridgeResponse(Actions.QR_SCAN, async () => {
     // TODO: 스캔한 QR 값(data.text)을 서버로 전송해 출석 처리
+    onSuccess?.();
     return {};
   });
 
