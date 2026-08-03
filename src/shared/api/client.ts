ba21117c0ef3ac5768c9cc68155db3f3c9af96b4
장강민 <jkm9780533@gmail.com>
2026-08-03 15:33:41 +0900

    feat: custom axios 세팅

---
+++ src/shared/api/client.ts
@@ -0,0 +1,18 @@
+import axios, { type CreateAxiosDefaults } from "axios";
+
+export const client = (config: CreateAxiosDefaults) => {
+  const client = axios.create({
+    timeout: 10000,
+    ...config,
+  });
+
+  client.interceptors.response.use(
+    response => response,
+    error => {
+      console.error(`[API Error] ${error.config?.url}`, error);
+      return Promise.reject(error);
+    },
+  );
+
+  return client;
+};
