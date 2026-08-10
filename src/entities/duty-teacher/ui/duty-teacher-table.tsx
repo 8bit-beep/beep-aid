c6114ad3aa9382c230f183f46d90c401790dd574
장강민 <jkm9780533@gmail.com>
2026-08-10 18:08:54 +0900

    refactor: shadow delete

--- src/entities/duty-teacher/ui/duty-teacher-table.tsx
+++ src/entities/duty-teacher/ui/duty-teacher-table.tsx
@@ -11,7 +11,7 @@ export const DutyTeacherTable = () => {
   const { dutyTeachers } = useDutyTeachers();
 
   return (
-    <section className="rounded-medium bg-white p-5 shadow-sm">
+    <section className="rounded-medium bg-white p-5">
       <h2 className="text-lg font-bold text-gray-900">오늘의 당직 선생님</h2>
 
       <div className="mt-4 grid grid-cols-3">
