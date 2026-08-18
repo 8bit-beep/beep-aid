65ca698e282d17a2036c13cc9b55c637a38ebac0
gituser <danjee2009@gmail.com>
2026-08-18 15:07:12 +0900

    fix: 로고 이미지 경로 수정

--- src/pages/home/ui/home-header.tsx
+++ src/pages/home/ui/home-header.tsx
@@ -7,7 +7,7 @@ export const HomeHeader = () => {
 
   return (
     <header className="flex items-center justify-between px-1 py-3">
-      <img src="/inappLogo.png" alt="삑" className="h-8 w-auto" />
+      <img src={`${import.meta.env.BASE_URL}beepLogo.svg`} alt="삑" className="h-8 w-auto" />
       <button type="button" onClick={openInfo} aria-label="앱 정보" className="h-9 w-9">
         <img src={infoIcon} alt="info" className="h-9 w-9" />
       </button>
